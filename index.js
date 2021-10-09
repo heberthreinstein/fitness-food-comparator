68e1ff95b6a7bf3d095c9373be9904ed76d8a70f
Heberth Reinstein <heberth.reinstein@gmail.com>
2021-10-09 20:42:01 +0000

    Saving product data on mongodb

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { MongoClient } = require('mongodb');
 
 const app = require('express')();
 const PORT = 8080;
 
 url = 'https://world.openfoodfacts.org';
 var productsList;
-var products = []
 var count = 0;
 var $;
+
+const uri = "mongodb+srv://admin:OTkD1kqPU1AI0yfX@cluster0.jxoyx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect()
+    .then(() => console.log("Connected to the database"))
+    .catch(err => console.log(err));
+
+const collection = client.db("fitness-food-comparator").collection("products");
 
 async function getProductsList(){
     return await axios.get(url).then(response => {
@@ -40,27 +48,40 @@ async function getProductData(productUrl){
 }
 
 async function getProductJson(position){
-    const a = $(productsList[position]).children('a');
-    const title = a.attr('title');
-    const productUrl = a.attr('href');
-    const status = 'draft';
-    console.log('Getting data of ' + title);
-    await getProductData(url + productUrl).then((productData) => {
-        products.push({title, productUrl, ...productData});
-        console.log('Successful aquired product data')
-    }).catch((err) => {
-        console.log('Error aquiring product data')
-        console.log(err);
-        products.push({title, productUrl, status});
-    });
+    if (position < productsList.length) { 
+        const a = $(productsList[position]).children('a');
+        const title = a.attr('title');
+        const productUrl = a.attr('href');
+        const status = 'draft';
+        console.log('Getting data of ' + title);
+        product = await getProductData(url + productUrl).then((productData) => {
+            console.log('Successful aquired product data')
+            return {title, productUrl, ...productData};
+        }).catch((err) => {
+            console.log('Error aquiring product data')
+            console.log(err);
+            return {title, productUrl, status};
+        });
+        await saveProduct(product);
+    }
+    if (count < 9) await getProductJson(++count);
+}
 
-    if (count < 5) await getProductJson(++count);
+async function saveProduct(product) {
+    var query = { barcode: product.barcode }
+    await collection.findOneAndReplace(query, product, { upsert: true })
+        .then(() => console.log('Saved product ' + product.title))
+        .catch(err => console.log(err))
+
 }
 
 getProductsList().then((list) => {
     productsList = list;
     getProductJson(0).then( () => {
-        console.log(products);
+        client.close().then( () => {
+            console.log("Disconnected from the database");
+            console.log("======Importation Finished======");
+        });
     });
 }).catch(err => console.log(err))
 
