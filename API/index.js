8093c1bf154aca6f34001a240f0e6d818bec55bf
Heberth Reinstein <heberth.reinstein@gmail.com>
2021-10-10 16:26:28 +0000

    API

--- API/index.js
+++ API/index.js
@@ -1,7 +1,21 @@
 const app = require('express')();
+const mongodb = require('../API/mongodb');
+
 const PORT = 8080;
 
-app.listen(
-    PORT,
-    () => console.log(`Running on port: ${PORT}`)
-)
+app.listen(PORT, () => console.log(`API Running on port ${PORT}`))
+
+app.get('/', (req, res) => {
+    res.status(200).send('Fullstack Challenge 2021!')
+})
+
+app.get('/products', async (req, res) => {
+    const products = await mongodb.collection.find().toArray();
+    res.send(products);
+})
+
+app.get('/products/:code', async (req, res) => {
+    querry = {barcode: req.params.code}
+    const products = await mongodb.collection.findOne(querry);
+    res.send(products);
+})
