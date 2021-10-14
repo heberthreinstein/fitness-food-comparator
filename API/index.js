25e5ce2f27e7e65927d1635669819d37c1440fb3
Heberth Reinstein <heberth.reinstein@gmail.com>
2021-10-14 00:21:46 +0000

    api access control

--- API/index.js
+++ API/index.js
@@ -14,18 +14,18 @@ cron.schedule('0 3 * * *', function(){
 app.listen(PORT, () => console.log(`API Running on port ${PORT}`))
 
 app.get('/', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200/');
     res.status(200).send('Fullstack Challenge 2021!')
 })
 
 app.get('/products', async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200/');
     const products = await mongodb.collection.find().toArray();
     res.send(products);
 })
 
 app.get('/products/:code', async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200/');
     querry = {barcode: req.params.code}
     const products = await mongodb.collection.findOne(querry);
     res.send(products);
